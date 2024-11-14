a6ff57f6d344b371b4beaf23dc7d8ed5f8777a0a
hassan <hassan.saab@harness.io>
2024-11-14 17:25:47 -0500

    add harness terraform

--- react/onboarding.tf
+++ react/onboarding.tf
@@ -9,21 +9,21 @@ terraform {
 provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = "Dn2AsSxZSmKVAq-WNhAl_w"
-  platform_api_key = "redact"
+  platform_api_key = var.harness_api_key
 }
 
-resource "harness_platform_project" "test" {
-  identifier = "testproject"
-  name       = "Test Project"
-  org_id     = "org_id"
+resource "harness_platform_project" "project" {
+  identifier = var.project_name
+  name       = var.project_name
+  org_id     = "default"
   color      = "#0063F7"
 }
 
-resource "harness_platform_pipeline" "new_service_pipeline" {
-  identifier      = "new_service_pipeline"
+resource "harness_platform_pipeline" "react_app_pipeline" {
+  identifier      = "react_app_pipeline"
   org_id          = "default"
   project_id      = "default_project"
-  name            = "new_service_pipeline"
+  name            = "react_app_pipeline"
   import_from_git = true
 
   git_import_info {
@@ -34,7 +34,7 @@ resource "harness_platform_pipeline" "new_service_pipeline" {
   }
 
   pipeline_import_request {
-    pipeline_name        = "new_service_pipeline"
+    pipeline_name        = "react_app_pipeline"
     pipeline_description = "Pipeline Description"
   }
 }
